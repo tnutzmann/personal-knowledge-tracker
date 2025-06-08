16d8d0ac870533ab621e3a516074e804e7d8f6a2
tnutzmann <tonynutzmann@icloud.com>
2025-06-08 14:32:11 +0200

    feat: generate SpringBoot Backend Project

---
+++ services/backend/src/test/java/de/tnutzmann/personal_knowledge_tracker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tnutzmann.personal_knowledge_tracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
